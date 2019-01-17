8685b1f896dc825123a3e1582e2659b3537f7f15
raniere.costa <raniere.costa@totvs.com.br>
2019-01-17 17:51:07 -0200

    Commit inicial - criação do projeto

---
+++ src/main/java/com/ranierecosta/workshopmongoDB/WorkshopmongoDbApplication.java
@@ -0,0 +1,14 @@
+package com.ranierecosta.workshopmongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoDbApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/ranierecosta/workshopmongoDB/WorkshopmongoDbApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ranierecosta.workshopmongoDB;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorkshopmongoDbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+

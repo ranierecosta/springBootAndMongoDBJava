4cf8ef8aef504e62dab5a30a88da4dc8e3aafe35
raniere.costa <raniere.costa@totvs.com.br>
2019-01-18 17:16:02 -0200

    Inserção de usuários com método POST

--- src/main/java/com/ranierecosta/workshopmongoDB/services/UserService.java
+++ src/main/java/com/ranierecosta/workshopmongoDB/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ranierecosta.workshopmongoDB.domain.User;
+import com.ranierecosta.workshopmongoDB.dto.UserDTO;
 import com.ranierecosta.workshopmongoDB.repository.UserRepository;
 import com.ranierecosta.workshopmongoDB.services.exception.ObjectNotFoundException;
 
@@ -24,6 +25,14 @@ public class UserService {
 		Optional<User> user = repository.findById(id);
 		
 		return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado!"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 	
 }

e8871659ae6343c48428d5d8c8e4773ad70a1d15
raniere.costa <raniere.costa@totvs.com.br>
2019-01-18 17:35:01 -0200

    Atualização de usuário com método PUT

--- src/main/java/com/ranierecosta/workshopmongoDB/services/UserService.java
+++ src/main/java/com/ranierecosta/workshopmongoDB/services/UserService.java
@@ -38,6 +38,20 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		
+		Optional<User> newObj = repository.findById(obj.getId());
+		
+		updateData(newObj.get(), obj);
+		
+		return repository.save(newObj.get());
+	}
+	
+	private void updateData(User newObj, User obj) {		
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}

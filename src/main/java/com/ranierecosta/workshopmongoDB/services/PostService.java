f2577dcfd5e608b01de9886144d4d6686d729c33
raniere.costa <raniere.costa@totvs.com.br>
2019-01-21 18:49:32 -0200

    Consultas simples com query methods spring/mongodb

--- src/main/java/com/ranierecosta/workshopmongoDB/services/PostService.java
+++ src/main/java/com/ranierecosta/workshopmongoDB/services/PostService.java
@@ -1,5 +1,6 @@
 package com.ranierecosta.workshopmongoDB.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,10 @@ public class PostService {
 		Optional<Post> post = repository.findById(id);
 		
 		return post.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado!"));
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repository.findByTitleContainingIgnoreCase(text);
 	}
 		
 }

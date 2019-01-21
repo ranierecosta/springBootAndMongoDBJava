9dbcbfd6bb05c3e69f33eba1387b92b2b1478411
raniere.costa <raniere.costa@totvs.com.br>
2019-01-21 17:46:10 -0200

    Endpoint para retornar todos os posts do usuário

--- src/main/java/com/ranierecosta/workshopmongoDB/resources/UserResource.java
+++ src/main/java/com/ranierecosta/workshopmongoDB/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.ranierecosta.workshopmongoDB.domain.Post;
 import com.ranierecosta.workshopmongoDB.domain.User;
 import com.ranierecosta.workshopmongoDB.dto.UserDTO;
 import com.ranierecosta.workshopmongoDB.services.UserService;
@@ -78,6 +79,15 @@ public class UserResource {
 		obj = service.update(obj);
 		
 		return ResponseEntity.noContent().build();
+		
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj.getPosts());
 		
 	}
 	
